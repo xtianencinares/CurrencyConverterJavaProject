c56dba3bc89dfa5461e5c257ac5df9cdb306cee7
Christian Encinares Bocz <144127538+xtianencinares@users.noreply.github.com>
2023-09-14 12:08:55 +0800

    Update CurrencyConverter.java

--- src/currencyConverter/CurrencyConverter.java
+++ src/currencyConverter/CurrencyConverter.java
@@ -15,7 +15,7 @@ public class CurrencyConverter {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					// Create and show main window at startup
+				       // Create and show main window at startup
 					 MainWindow mainWindow = new MainWindow();
 					 mainWindow.setVisible(true);
 				} catch (Exception e) {
