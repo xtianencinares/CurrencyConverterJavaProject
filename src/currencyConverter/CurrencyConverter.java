b301705898ba7f7f68a506f69173ab7351b0bf51
Christian Encinares Bocz <144127538+xtianencinares@users.noreply.github.com>
2023-12-09 11:42:07 +0800

    Update CurrencyConverter.java

--- src/currencyConverter/CurrencyConverter.java
+++ src/currencyConverter/CurrencyConverter.java
@@ -4,7 +4,7 @@ import java.awt.EventQueue;
 import javax.swing.UIManager;
 
 public class CurrencyConverter {
-
+ 
 	public static void main(String[] args) {
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
